e2238c889e6c5680aee76c74b0f3f0fee827e92e
Ryo Utsunomiya <millionsnowliving@gmail.com>
2016-12-31 11:09:38 +0900

    Add HyperLink.aspx

---
+++ SelfAspNetCSharp/HyperLink.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="HyperLink.aspx.cs" Inherits="SelfAspNetCSharp.HyperLink" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+        <asp:HyperLink ID="link" runat="server" ImageUrl="http://www.wings.msn.to/image/wings.jpg" NavigateUrl="http://wings.msn.to/">WINGS</asp:HyperLink>
+        <p>
+            <asp:Image ID="logo" runat="server" AlternateText="WINGS Logo" ImageUrl="http://www.wings.msn.to/image/wings.jpg" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ SelfAspNetCSharp/HyperLink.aspx.designer.cs
@@ -0,0 +1,42 @@
+﻿//------------------------------------------------------------------------------
+// <自動生成>
+//     このコードはツールによって生成されました。
+//
+//     このファイルへの変更は、以下の状況下で不正な動作の原因になったり、
+//     コードが再生成されるときに損失したりします。 
+// </自動生成>
+//------------------------------------------------------------------------------
+
+namespace SelfAspNetCSharp {
+    
+    
+    public partial class HyperLink {
+        
+        /// <summary>
+        /// form1 コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// link コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink link;
+        
+        /// <summary>
+        /// logo コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image logo;
+    }
+}
