b64fbdc6d4ace2350b32951ee02588b7c25a1d42
Ryo Utsunomiya <millionsnowliving@gmail.com>
2017-02-18 15:31:13 +0900

    Add NonAccess

---
+++ SelfAspNetCSharp/NonAccess.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SelfAspNetCSharp
+{
+    public partial class NonAccess : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var setting = ConfigurationManager.ConnectionStrings["SelfAsp"];
+            var adapter = new SqlDataAdapter("SELECT * FROM Book WHERE publish = @publish", setting.ConnectionString);
+            adapter.SelectCommand.Parameters.AddWithValue("@publish", "翔泳社");
+            var ds = new DataSet();
+            adapter.Fill(ds, "Book");
+
+            for (int i = 0; i < ds.Tables["Book"].Rows.Count; i++)
+            {
+                Response.Write(ds.Tables["Book"].Rows[i]["title"] + "<br />");
+            }
+        }
+    }
+}
