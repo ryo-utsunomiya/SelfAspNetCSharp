93e82edc2a72dd3860ec0f07174b59ed24328aed
Ryo Utsunomiya <millionsnowliving@gmail.com>
2017-02-18 16:31:36 +0900

    Add ObjectParam

---
+++ SelfAspNetCSharp/Album.cs
@@ -0,0 +1,67 @@
+﻿using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SelfAspNetCSharp
+{
+    public class Album
+    {
+        private const string ConnectionName = "SelfAsp";
+
+        [DataObjectMethod(DataObjectMethodType.Select, true)]
+        public DataSet GetAlbumData(string category)
+        {
+            var command = new SqlCommand()
+            {
+                Connection = CreateSqlConnection(ConnectionName)
+            };
+
+            const string sql = "SELECT aid, comment, updated, favorite, category FROM Album";
+
+            if (category == "無選択")
+            {
+                command.CommandText = sql;
+            }
+            else
+            {
+                command.CommandText = sql + " WHERE category = @category";
+                command.Parameters.AddWithValue("@category", category);
+            }
+
+            var ds = new DataSet();
+            new SqlDataAdapter(command).Fill(ds, "Album");
+
+            return ds;
+        }
+
+        [DataObjectMethod(DataObjectMethodType.Delete, true)]
+        public int DeleteAlbumData(string aid)
+        {
+            var connection = CreateSqlConnection(ConnectionName);
+            var command = new SqlCommand("DELETE FROM Album WHERE aid = @aid", connection);
+            command.Parameters.AddWithValue("@aid", aid);
+
+            return ExecuteNonQuery(connection, command);
+        }
+
+        private static string GetConnectionString(string connectionName)
+        {
+            return ConfigurationManager.ConnectionStrings[connectionName].ConnectionString;
+        }
+
+        private static SqlConnection CreateSqlConnection(string connectionName)
+        {
+            return new SqlConnection(GetConnectionString(connectionName));
+        }
+
+        private static int ExecuteNonQuery(IDbConnection connection, IDbCommand command)
+        {
+            connection.Open();
+            int rows = command.ExecuteNonQuery();
+            connection.Close();
+
+            return rows;
+        }
+    }
+}
---
+++ SelfAspNetCSharp/ObjectParam.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ObjectParam.aspx.cs" Inherits="SelfAspNetCSharp.ObjectParam" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:RadioButtonList ID="list" runat="server" AppendDataBoundItems="True" AutoPostBack="True" DataSourceID="sds_list" DataTextField="category" DataValueField="category" RepeatDirection="Horizontal">
+            <asp:ListItem Selected="True">無選択</asp:ListItem>
+        </asp:RadioButtonList>
+        <asp:SqlDataSource ID="sds_list" runat="server" ConnectionString="<%$ ConnectionStrings:SelfAsp %>" SelectCommand="SELECT DISTINCT [category] FROM [Album] ORDER BY [category]"></asp:SqlDataSource>
+    
+    </div>
+        <asp:GridView ID="grid" runat="server" DataKeyNames="aid" DataSourceID="ods" EmptyDataText="カテゴリは選択されていません。">
+            <Columns>
+                <asp:CommandField ShowDeleteButton="True" />
+            </Columns>
+        </asp:GridView>
+        <asp:ObjectDataSource ID="ods" runat="server" DeleteMethod="DeleteAlbumData" SelectMethod="GetAlbumData" TypeName="SelfAspNetCSharp.Album">
+            <DeleteParameters>
+                <asp:Parameter Name="aid" Type="String" />
+            </DeleteParameters>
+            <SelectParameters>
+                <asp:ControlParameter ControlID="list" Name="category" PropertyName="SelectedValue" Type="String" />
+            </SelectParameters>
+        </asp:ObjectDataSource>
+    </form>
+</body>
+</html>
