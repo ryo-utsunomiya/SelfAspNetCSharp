c1d35653f2cf364f6960d6bb95946a5cd76a77b1
Ryo Utsunomiya <millionsnowliving@gmail.com>
2017-03-22 22:37:06 +0900

    Add ViewStateGame

---
+++ SelfAspNetCSharp/ViewStateGame.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ViewStateGame.aspx.cs" Inherits="SelfAspNetCSharp.ViewStateGame" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+        <asp:Label ID="Label1" runat="server" Text="Input 1~100:"></asp:Label>
+        <asp:TextBox ID="txtNum" runat="server"></asp:TextBox>
+        <asp:Button ID="btnSend" runat="server" OnClick="btnSend_OnClick_Click" Text="Value" />
+        <p>
+            <asp:Label ID="lblResult" runat="server" Text=""></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
---
+++ SelfAspNetCSharp/ViewStateGame.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SelfAspNetCSharp
+{
+    public partial class ViewStateGame : System.Web.UI.Page
+    {
+        private readonly string count = "count";
+        private readonly string answer = "answer";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Page.IsPostBack) return;
+
+            ViewState[count] = 0;
+            ViewState[answer] = new Random().Next(100);
+        }
+
+        protected void btnSend_OnClick_Click(object sender, EventArgs e)
+        {
+            ViewState[count] = (int)ViewState[count] + 1;
+
+            if (ViewState[answer].ToString().Equals(txtNum.Text))
+            {
+                lblResult.Text = $"{ViewState[count]} times";
+                ViewState.Clear();
+            }
+            else
+            {
+                lblResult.Text = (int)ViewState[answer] > int.Parse(txtNum.Text) ?
+                    "More larger" : "More smaller";
+            }
+        }
+    }
+}
